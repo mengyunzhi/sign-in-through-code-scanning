05c198ac8c2cb488dc89c466259943a8ee27c761
858190647@qq.com <858190647@qq.com>
2022-03-04 15:10:58 +0800

    C_finish

---
+++ thinkphp/application/index/controller/AdminController.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\index\controller;
+use think\controller;
+
+/**
+ * 管理端
+ */
+class AdminController extends controller
+{
+	
+	public function AdminController() 
+	{
+		
+	}
+}
