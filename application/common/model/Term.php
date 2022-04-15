64988349dc4c47e96551b1376b4367d1c122e22f
858190647@qq.com <858190647@qq.com>
2022-04-15 14:00:33 +0800

    merge

--- application/common/model/Term.php
+++ application/common/model/Term.php
@@ -13,11 +13,23 @@ class Term extends Model {
     }
 
     public function getState() {
-        return isset($this->data['state']) ? $this->data['state'] : null;
+        if (isset($this->data['state'])) {
+            if ($this->state === 0) {
+                return '未激活';
+            } else {
+                return '已激活';
+            }
+        } else {
+            return null;
+        }
     }
 
     public function getStartTime() {
-        return isset($this->data['start_time']) ? $this->data['start_time'] : null;
+        return isset($this->data['startTime']) ? $this->data['startTime'] : $this->data['startTime'] = date('Y-m-d', $this->start_time);
+    }
+
+    public function getEndTime() {
+        return isset($this->data['endTime']) ? $this->data['endTime'] : $this->data['endTime'] = date('Y-m-d', $this->start_time);
     }
 
     static public function getCurrentTerm()
@@ -25,4 +37,28 @@ class Term extends Model {
         return self::where('state', 'eq', 1)->find();
     }
 
+    static public function termSave($name, $startTime, $endTime, $state, &$msg)
+    {
+        //检查数据
+        if (is_null($name)) {
+            throw new \Exception('name信息为空');
+        } elseif (is_null($startTime)) {
+            throw new \Exception('startTime');
+        } elseif (is_null($endTime)) {
+            throw new \Exception('endTime');
+        } elseif (is_null($state)) {
+            throw new \Exception('state');
+        }
+        //保存
+        $Term = new Term;
+        $Term->setAttr('name', $name);
+        $Term->start_time = strtotime($startTime);
+        $Term->end_time = strtotime($endTime);
+        $Term->state = $state;
+        $status = $Term->validate()->save();
+        //失败信息
+        $msg = $Term->getError();
+
+        return $status;
+    }
 }
