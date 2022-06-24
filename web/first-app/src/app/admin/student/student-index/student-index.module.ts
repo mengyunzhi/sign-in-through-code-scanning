4799cf9fa8ad5dfdca1b8a053d87c22b33d2e678
teacherpan <3792535@qq.com>
2022-06-24 12:35:55 +0800

    init student index module

---
+++ web/first-app/src/app/admin/student/student-index/student-index.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {StudentIndexComponent} from './student-index.component';
+import {PageModule} from '../../../page/page.module';
+import {RouterModule} from '@angular/router';
+
+@NgModule({
+  declarations: [StudentIndexComponent],
+  imports: [
+    CommonModule,
+    PageModule,
+    RouterModule
+  ],
+  exports: [
+    StudentIndexComponent
+  ]
+})
+export class StudentIndexModule { }
