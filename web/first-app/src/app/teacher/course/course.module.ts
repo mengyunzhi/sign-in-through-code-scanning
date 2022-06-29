85f7a547879d2632917494f45891c9ae347a83fc
chshihang <858190647@qq.com>
2022-06-29 17:34:54 +0800

    debug

--- web/first-app/src/app/teacher/course/course.module.ts
+++ web/first-app/src/app/teacher/course/course.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import {CourseRoutingModule} from './course-routing.module';
 import { CourseAddComponent } from './course-add/course-add.component';
 import { CourseEditComponent } from './course-edit/course-edit.component';
+import {CourseIndexComponent} from './course-index/course-index.component';
 
 
 
 @NgModule({
-  declarations: [CourseAddComponent, CourseEditComponent],
+  declarations: [CourseAddComponent, CourseEditComponent, CourseIndexComponent],
   imports: [
     CommonModule,
     CourseRoutingModule,
