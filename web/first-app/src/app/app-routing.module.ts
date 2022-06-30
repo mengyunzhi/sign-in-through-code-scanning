271f876df973a5d012bbd4319dcb7063afaf1e1a
zhangrui_glb <tute170231@163.com>
2022-06-30 21:30:11 +0800

    注销bug修改半成品

--- web/first-app/src/app/app-routing.module.ts
+++ web/first-app/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {WelcomeComponent} from './welcome/welcome.component';
+import {LoginComponent} from './login/login.component';
 
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: './login/',
     component: WelcomeComponent,
   },
   {
