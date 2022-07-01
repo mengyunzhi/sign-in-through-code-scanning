5b0704fb1ce198174ab9100f8342de068c0b6a74
zhangrui_glb <tute170231@163.com>
2022-07-01 11:23:15 +0800

    修改学期课程表 | 修改teacher端task路由

--- web/first-app/src/app/teacher/task/task.module.ts
+++ web/first-app/src/app/teacher/task/task.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {TaskRoutingModule} from './task-routing.module';
+import {CourseStartModule} from './course-start/course-start.module';
+import {SignInStateComponent} from './sign-in-state/sign-in-state.component';
+import {TaskIndexModule} from './task-index/task-index.module';
+import {ViewStudentModule} from './view-student/view-student.module';
+import {ViewStudentAddModule} from './view-student-add/view-student-add.module';
+import {ViewStudentFractionModule} from './view-student-fraction/view-student-fraction.module';
+import {SignInStateModule} from './sign-in-state/sign-in-state.module';
 
 
 
@@ -8,6 +15,12 @@ import {TaskRoutingModule} from './task-routing.module';
   imports: [
     CommonModule,
     TaskRoutingModule,
+    CourseStartModule,
+    SignInStateModule,
+    TaskIndexModule,
+    ViewStudentModule,
+    ViewStudentAddModule,
+    ViewStudentFractionModule
   ],
 })
 export class TaskModule { }
--- web/first-app/src/app/teacher/task/view-student-fraction/view-student-fraction.module.ts
+++ web/first-app/src/app/teacher/task/view-student-fraction/view-student-fraction.module.ts
@@ -1,20 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ViewStudentFractionComponent} from './view-student-fraction.component';
-import {RouterModule, Routes} from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: ViewStudentFractionComponent
-  }
-];
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [ViewStudentFractionComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule,
   ]
 })
 export class ViewStudentFractionModule { }
