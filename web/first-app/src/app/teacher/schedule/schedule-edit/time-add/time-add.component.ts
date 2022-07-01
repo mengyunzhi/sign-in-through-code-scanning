58db2b0a4707bd3f4defa96eafa3c0d869e0c6d6
HHHHHHHHP <792650995@qq.com>
2022-07-01 17:20:42 +0800

    p

--- web/first-app/src/app/teacher/schedule/schedule-edit/time-add/time-add.component.ts
+++ web/first-app/src/app/teacher/schedule/schedule-edit/time-add/time-add.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimeAddComponent implements OnInit {
 
+  lessons = [1, 2, 3, 4, 5];
+  days = ['一', '二', '三', '四', '五', '六', '日'];
+  weeks = [1, 2, 3, 4, 5, 6, 7, 8];
+  rooms = [1, 2, 3, 4, 5];
+
   constructor() { }
 
   ngOnInit(): void {
