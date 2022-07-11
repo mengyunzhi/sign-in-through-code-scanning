a70ccfce364f975d6756a4a620b446c9b4b3fb82
chshihang <858190647@qq.com>
2022-07-11 14:02:41 +0800

    347

--- web/first-app/src/app/teacher/schedule/schedule-edit/schedule-edit.component.spec.ts
+++ web/first-app/src/app/teacher/schedule/schedule-edit/schedule-edit.component.spec.ts
@@ -3,14 +3,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ScheduleEditComponent } from './schedule-edit.component';
 import {HttpClientModule} from '@angular/common/http';
 import {RouterTestingModule} from '@angular/router/testing';
-import {ScheduleEditModule} from './schedule-edit.module';
-import {Course} from '../../../entity/course';
-import {Schedule} from '../../../entity/schedule';
-import {Program} from '../../../entity/program';
-import {Clazz} from '../../../entity/clazz';
-import {Dispatch} from '../../../entity/dispatch';
-import {Room} from '../../../entity/room';
-
 describe('ScheduleEditComponent', () => {
   let component: ScheduleEditComponent;
   let fixture: ComponentFixture<ScheduleEditComponent>;
@@ -21,7 +13,6 @@ describe('ScheduleEditComponent', () => {
       imports: [
         HttpClientModule,
         RouterTestingModule,
-        ScheduleEditModule
       ]
     })
     .compileComponents();
@@ -33,42 +24,7 @@ describe('ScheduleEditComponent', () => {
     fixture.detectChanges();
   });
 
-  fit('should create', () => {
-    component.data = {
-      schedule: {
-        id: 1,
-        course: {
-          name: '测试文件班级1',
-          lesson: 11
-        } as Course,
-      } as Schedule,
-      programs: [
-        {
-          name: '测试文件项目1',
-          lesson: 44
-        } as Program,
-      ] as Program[],
-      clazzes: [
-        {
-          name: '测试文件班级1',
-        } as Clazz
-      ] as Clazz[],
-      dispatches: [
-        {
-          week: 1,
-          day: 2,
-          lesson: 3
-        } as Dispatch,
-      ],
-      rooms: [
-        [
-          {
-            name: '测试文件教室1',
-          } as Room,
-        ]
-      ]
-    };
+  it('should create', () => {
     expect(component).toBeTruthy();
-    console.log(component.data);
   });
 });
--- web/first-app/src/app/teacher/schedule/schedule.module.ts
+++ web/first-app/src/app/teacher/schedule/schedule.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ScheduleRoutingModule} from './schedule-routing.module';
-import { ScheduleEditComponent } from './schedule-edit/schedule-edit.component';
 import { ProgramAddComponent } from './schedule-edit/program-add/program-add.component';
 import { ClazzAddComponent } from './schedule-edit/clazz-add/clazz-add.component';
 import { TimeAddComponent } from './schedule-edit/time-add/time-add.component';
@@ -9,6 +8,7 @@ import { ProgramEditComponent } from './schedule-edit/program-edit/program-edit.
 import { NameEditComponent } from './schedule-edit/name-edit/name-edit.component';
 import { ScheduleAddComponent } from './schedule-add/schedule-add.component';
 import {ReactiveFormsModule} from '@angular/forms';
+import {ScheduleEditComponent} from './schedule-edit/schedule-edit.component';
 
 
 
@@ -19,7 +19,8 @@ import {ReactiveFormsModule} from '@angular/forms';
     TimeAddComponent,
     ProgramEditComponent,
     NameEditComponent,
-    ScheduleAddComponent
+    ScheduleAddComponent,
+    ScheduleEditComponent
   ],
   imports: [
     CommonModule,
