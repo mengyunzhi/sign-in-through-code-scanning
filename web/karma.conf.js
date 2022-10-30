60c2ea0234e4499fa980855853e54e2358fe3472
chshihang <858190647@qq.com>
2022-10-30 10:11:39 +0800

    1

--- web/karma.conf.js
+++ web/karma.conf.js
@@ -3,7 +3,7 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
+    basePath: 'first-app',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
