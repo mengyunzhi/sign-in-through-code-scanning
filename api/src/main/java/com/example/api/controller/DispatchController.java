18c52574ba4fd50815df3dfb2bed28ff7ae9fb0a
HHepan <250409258@qq.com>
2022-12-02 11:54:41 +0800

    教师端=》课程表=》getData

--- api/src/main/java/com/example/api/controller/DispatchController.java
+++ api/src/main/java/com/example/api/controller/DispatchController.java
@@ -2,10 +2,13 @@ package com.example.api.controller;
 
 
 import com.example.api.entity.Dispatch;
+import com.example.api.entity.Schedule;
+import com.example.api.entity.forType.forCourseScheduleGetData.ForCourseScheduleGetData;
 import com.example.api.service.DispatchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,6 +26,14 @@ public class DispatchController {
     @GetMapping("getAll")
     public List<Dispatch> getAll() {
         return this.dispatchService.getAll();
+    }
+
+    /*
+    * 教师端 => 课程表 => 获取课程表所需数据
+    * */
+    @GetMapping("getData")
+    public ForCourseScheduleGetData getData(@RequestParam String userNumber) {
+        return this.dispatchService.getData(userNumber);
     }
 
 }
